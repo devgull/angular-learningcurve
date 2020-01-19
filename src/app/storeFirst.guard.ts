9880d6f0aa00a72884614237714ece1dcea075ed
devgull <dusan@todorovic.co>
2020-01-19 15:53:46 +0100

    3 files modified, 2 added

--- src/app/storeFirst.guard.ts
+++ src/app/storeFirst.guard.ts
@@ -1,49 +1,33 @@
 import { Injectable } from "@angular/core";
 
 import {
-
-    ActivatedRouteSnapshot, RouterStateSnapshot,
-
-    Router
-
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
 } from "@angular/router";
 
 import { StoreComponent } from "./store/store.component";
 
-
-
 @Injectable()
-
 export class StoreFirstGuard {
+  private firstNavigation = true;
 
-    private firstNavigation = true;
+  constructor(private router: Router) {}
 
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.firstNavigation) {
+      this.firstNavigation = false;
 
+      if (route.component != StoreComponent) {
+        this.router.navigateByUrl("/");
 
-    constructor(private router: Router) { }
-
-
-
-    canActivate(route: ActivatedRouteSnapshot,
-
-        state: RouterStateSnapshot): boolean {
-
-        if (this.firstNavigation) {
-
-            this.firstNavigation = false;
-
-            if (route.component != StoreComponent) {
-
-                this.router.navigateByUrl("/");
-
-                return false;
-
-            }
-
-        }
-
-        return true;
-
+        return false;
+      }
     }
 
+    return true;
+  }
 }
