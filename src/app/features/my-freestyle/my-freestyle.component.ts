318e87fb397a2151960d4192853bdda05a6fb522
devgull <dusan@todorovic.co>
2020-03-17 14:21:57 +0100

    1 file modified, 3 added

---
+++ src/app/features/my-freestyle/my-freestyle.component.ts
@@ -0,0 +1,11 @@
+import { OnInit, Component, ViewChild, Injector, AfterViewInit, ElementRef, TemplateRef } from "@angular/core";
+
+@Component({
+    selector: 'my-freestyle',
+    templateUrl: 'my-freestyle.component.html',
+    styleUrls: ['my-freestyle.component.scss']
+})
+
+export class MyFreestyleComponent{
+
+}
