a5c0672e2b2209884e0807fdefb26b79a7de1f76
devgull <dusan@todorovic.co>
2020-03-17 14:36:50 +0100

    1 file modified

--- src/app/features/my-freestyle/my-freestyle.component.ts
+++ src/app/features/my-freestyle/my-freestyle.component.ts
@@ -13,4 +13,43 @@ export class MyFreestyleComponent{
         this.qrValue = "QR Code";
     }
 
+  private downloadQRCode() {
+
+        let base64String: string = document.getElementsByTagName("qr-code")[0].getElementsByTagName('img')[0].src.split(',')[1];
+        const blobData = this.convertBase64ToBlobData(base64String);
+        const filename: string = "qrcode.png";
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) { //IE
+            window.navigator.msSaveOrOpenBlob(blobData, filename);
+        } else { // chrome
+            const blob = new Blob([blobData], { type: 'image/png' });
+            const url = window.URL.createObjectURL(blob);
+            // window.open(url);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = filename;
+            link.click();
+        }
+    }
+
+    convertBase64ToBlobData(base64Data: string, contentType: string = 'image/png', sliceSize = 512) {
+        const byteCharacters = atob(base64Data);
+        const byteArrays = [];
+
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            const byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            const byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+        const blob = new Blob(byteArrays, { type: contentType });
+        return blob;
+    }
+
 }
